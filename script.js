7a856fa4ee7893ff5c9f20fe45f8dd0cebde602f
Matheus Freitas <47475180+iMatheusouza@users.noreply.github.com>
2019-10-03 16:39:44 -0300

    Working Update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+document.getElementById('validateButton').addEventListener('click', check);
+
 function check() {
   var year = new Date()
   var currentYear = year.getFullYear()
   var yourYear = document.getElementById('birthYear')
   var yourAge = currentYear - yourYear.value
   var formSex = document.querySelector('input#masc')
-  var generateContent = document.querySelector('div#newContent').innerHTML
 
 
   if (yourYear.value.length == 0 || Number(yourYear.value) > currentYear 
@@ -12,54 +13,103 @@ function check() {
     window.alert('[ERRO] Informações inválidas')
   } 
   else {
-    var generateContent = document.createElement('img')
     if(formSex.checked == true) {
       if (yourAge >= 0 && yourAge <= 14) {
         //CRIANÇA
-        generateContent.setAttribute('src', 'boy.jpg' )
+        var generateText = document.getElementById('newText')
+        generateText.innerText = `Identificamos um homem de ${yourAge} anos` 
+
+        var generateImage = document.createElement('img')
+        generateImage.setAttribute('src', 'boy.jpg' )
+        generateImage.style.borderRadius = '50%'
+        document.getElementById('newContent').appendChild(generateImage)
         
       }
       else if (yourAge < 30) {
-        document.write('Jovem')
+        var generateText = document.getElementById('newText')
+        generateText.innerText = `Identificamos um homem de ${yourAge} anos` 
+        
+        var generateImage = document.createElement('img')
+        generateImage.setAttribute('src', 'young-man.jpg' )
+        generateImage.style.borderRadius = '50%'
+        document.getElementById('newContent').appendChild(generateImage)
 
       }
       else if (yourAge < 60) {
-        document.write('Adulto')
+        var generateText = document.getElementById('newText')
+        generateText.innerText = `Identificamos um homem de ${yourAge} anos` 
 
+        var generateImage = document.createElement('img')
+        generateImage.setAttribute('src', 'adult-man.jpg' )
+        generateImage.style.borderRadius = '50%'
+        document.getElementById('newContent').appendChild(generateImage)
       }
       else {
-        document.write('Idoso')
-        
+        var generateText = document.getElementById('newText')
+        generateText.innerText = `Identificamos um homem de ${yourAge} anos` 
+
+        var generateImage = document.createElement('img')
+        generateImage.setAttribute('src', 'old-man.jpg' )
+        generateImage.style.borderRadius = '50%'
+        document.getElementById('newContent').appendChild(generateImage) 
       }
-    
-    
-    
-    // MULHER 
-    
+
+    // MULHER
     } 
-    else {
-      if (yourAge >= 0 && yourAge <= 14) {
-        document.write('criança')
-      }
-      else if (yourAge < 30) {
-        document.write('Jovem')
-
-      }
-      else if (yourAge < 60) {
-        document.write('Adulto')
-
-      }
       else {
-        document.write('Idoso')
-        
-      }
-    
+        if (yourAge >= 0 && yourAge <= 14) {
+          var generateText = document.getElementById('newText')
+          generateText.innerText = `Identificamos uma mulher de ${yourAge} anos` 
+  
+          var generateImage = document.createElement('img')
+          generateImage.setAttribute('src', 'girl.jpg' )
+          generateImage.style.borderRadius = '50%'
+          document.getElementById('newContent').appendChild(generateImage) 
+        }
+
+        else if (yourAge < 30) {
+          var generateText = document.getElementById('newText')
+          generateText.innerText = `Identificamos uma mulher de ${yourAge} anos` 
+  
+          var generateImage = document.createElement('img')
+          generateImage.setAttribute('src', 'young-woman.jpg' )
+          generateImage.style.borderRadius = '50%'
+          document.getElementById('newContent').appendChild(generateImage) 
+        }
+
+        else if (yourAge < 60) {
+          var generateText = document.getElementById('newText')
+          generateText.innerText = `Identificamos uma mulher de ${yourAge} anos` 
+  
+          var generateImage = document.createElement('img')
+          generateImage.setAttribute('src', 'adult-woman.jpg' )
+          generateImage.style.borderRadius = '50%'
+          document.getElementById('newContent').appendChild(generateImage) 
+        }
+
+        else {
+          var generateText = document.getElementById('newText')
+          generateText.innerText = `Identificamos uma mulher de ${yourAge} anos` 
+  
+          var generateImage = document.createElement('img')
+          generateImage.setAttribute('src', 'old-woman.jpg' )
+          generateImage.style.borderRadius = '50%'
+          document.getElementById('newContent').appendChild(generateImage) 
+        }
+      
       }
       
 
   }
-  
 
+/*removeEventListener vai remover a função de gerar elementos
+do botão, fazendo assim não ser possível gerar várias imagens
+e quebrar o site*/
 
+  document.getElementById('validateButton').removeEventListener('click', check)
 
+}
+
+function refresh () {
+  window.location.reload()
 }
